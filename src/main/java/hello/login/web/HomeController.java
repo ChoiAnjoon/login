5d241a0e18b484c0aaba647ec4c5e487a6de0d82
choi <dkswnsl12@naver.com>
2023-06-22 21:20:10 +0900

    cookie로 login 기능 구현

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -1,15 +1,41 @@
 package hello.login.web;
 
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping("/")
+    private final MemberRepository memberRepository;
+
+    //    @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/")
+    // required = false도 넣어주어야 한다. 왜냐하면 로그인 안한 사용자도 들어와야 함으로
+    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
+
+        if (memberId == null) {
+            return "home";
+        }
+
+        // 로그인에 성공한 사용자 Cookie가 있는 사용자
+        Member loginMember = memberRepository.findById(memberId);
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
     }
 }
